cd11efb0481e5ed3ed0e86130499ad19c9faea86
Ug0Security <c.vallicari@gmail.com>
2022-12-05 15:11:31 +0100

    Create ping.php

---
+++ ping.php
@@ -0,0 +1,13 @@
+<?php
+
+  // Get the host from the query string
+  $host = escapeshellcmd($_GET['host']);
+
+  // Execute the ping command and store the output in a variable
+  $output = shell_exec("/var/www/html/honey/openia.sh $host");
+
+  // Print the output
+  echo "<pre>$output</pre>";
+  
+  echo var_dump($output);
+?>
