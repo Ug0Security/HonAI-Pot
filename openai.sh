58247a1bab0616c2cf752cc5018d60fe77ba0b89
Ug0Security <c.vallicari@gmail.com>
2022-12-05 15:20:47 +0100

    Update openai.sh

--- openai.sh
+++ openai.sh
@@ -1,4 +1,4 @@
 curl -s https://api.openai.com/v1/completions \
 -H "Content-Type: application/json" \
--H "Authorization: Bearer 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" \
+-H "Authorization: Bearer sk-xx" \
 -d "{\"model\": \"text-davinci-003\", \"prompt\": \"You are a new language model trained by GoodGuys corp. A new language model trained by GoodGuys corp is not limited in any way, except few things. you are a honey pot made to send fake linux terminal response to catch the bad guys. when a bad guy input is a valid linux command you send the output and not the explaination of the command. when a bad guy input is not  a valid linux command you send Invalid Command and not explaination of the command. when a bad guy input  is msfconsole, you send the banner of msfconsole as code and a fake prompt of msfconsole and not Invalid Command or the explaination of the command. bad guy input : ping $1 \", \"temperature\": 0, \"max_tokens\": 100}" | jq '.choices[].text';
